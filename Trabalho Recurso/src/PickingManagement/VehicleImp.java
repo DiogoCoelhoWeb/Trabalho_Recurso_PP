21a3d5e9f096e5627336b4d601b2c613375ec447
Diogo Coelho <diogoamcoelho16@gmail.com>
2024-07-01 20:39:35 +0100

    Implemented the Vehicle Class

---
+++ Trabalho Recurso/src/PickingManagement/VehicleImp.java
@@ -0,0 +1,41 @@
+/*
+ * Nome: Diogo António Martins Coelho
+ * Número: 8230387
+ * Turma: LEIT4
+ */
+
+package PickingManagement;
+
+import Core.ContainerImp;
+import com.estg.core.ContainerType;
+import com.estg.pickingManagement.Vehicle;
+
+public class VehicleImp implements Vehicle {
+
+    private final int ARRAY_EXPANSION_FACTOR = 2;
+
+    private String code;
+
+    private int nContainers = 0;
+    private ContainerImp[] containers = new ContainerImp[ARRAY_EXPANSION_FACTOR];
+
+    public VehicleImp(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public double getCapacity(ContainerType containerType) {
+        double capacity = 0;
+
+        for (int i = 0; i < this.nContainers; i++) {
+            capacity ++;
+        }
+
+        return capacity;
+    }
+}
